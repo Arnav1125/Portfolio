c5c5a9a842c7ebc8d63450e79904bd3372151c89
Arnav Gurung <142315650+Arnav1125@users.noreply.github.com>
2024-07-16 14:01:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click',function(e){
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+
+
+    });
+});
